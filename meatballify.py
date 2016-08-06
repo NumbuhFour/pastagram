dc4678cdd385af68a3ed5c2af63b66ed80c80dba
Frankie DiPietro <fad4470@rit.edu>
2016-08-06 09:46:47 -0400

    More tests

--- meatballify.py
+++ meatballify.py
@@ -24,7 +24,7 @@ meatballCV = cv2.imread('meatball.png')
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-faces = face_cascade.detectMultiScale(gray, 1.8, 7)
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 for(x, y, w, h) in faces:
 	cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0),2)
 	roi_gray = gray[y:y+h, x:x+w]
